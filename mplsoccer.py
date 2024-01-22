e8fb66e84415b632f24ab099f7ec6daec180a349
Szymok <szymonkowal019@gmail.com>
2024-01-23 00:16:58 +0100

    Stats per team

--- mplsoccer.py
+++ mplsoccer.py
@@ -508,4 +508,20 @@ if all_teams_selected == 'Include all available teams':
         st.markdown(" ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎"+str(df_match_result.iloc[1]['offside']))
         st.markdown(" ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎"+str(df_match_result.iloc[1]['corners']))
 
+### TEAM ###
+row4_spacer1, row4_1, row4_spacer2 = st.columns((.2, 7.1, .2))
+with row4_1:
+    st.subheader('Analysis per Team')
+row5_spacer1, row5_1, row5_spacer2, row5_2, row5_spacer3 = st.columns((.2, 2.3, .4, 4.4, .2))
+with row5_1:
+    st.markdown('Investigate a variety of stats for each team. Which team scores the most goals per game? How deas your team compare in terms of distance ran per game?')
+    plot_x_per_team_selected = st.selectbox('Which attribute do you want to analyze?', list(label_attr_dict_teams.keys()), key='attribute_team')
+    plot_x_per_team_type = st.selectbox('Which measure do you want to analyze?', types, key = 'measure_team')
+    specific_team_colors = st.checkbox('Use team specific color scheme')
+with row5_2:
+    if all_teams_selected != 'Select teams manually (choose below)' or selected_teams:
+        plot_x_per_team(plot_x_per_team_selected, plot_x_per_team_type)
+    else:
+        st.warning('Please select at least one team')
 
+### SEASON ###
